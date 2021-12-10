24ff57b5e6bce8b5b0048be8ba6dc043c98766fb
Tim Franssen <mail@timendus.com>
2021-12-10 13:59:15 +0100

    Add game example (ugly)

--- JavaScript and the browser/6. dom manipulation/script.js
+++ JavaScript and the browser/6. dom manipulation/script.js
@@ -1,26 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('button');
+
   const section = document.querySelector('section');
-  const counter = document.getElementById('counter');
-  let count = 0;
+  const userChoice = document.querySelector('#user_choice');
+  const compChoice = document.querySelector('#comp_choice');
+  const result = document.querySelector('#result');
 
-  document.addEventListener('click', e => {
-    console.log("Document clicked!");
+  const scores = {
+    human: 0,
+    computer: 0
+  };
+
+  const choices = [
+    {
+      name: 'Rock',
+      icon: 'fa-hand-rock',
+      beats: 'Scissors'
+    },
+    {
+      name: 'Paper',
+      icon: 'fa-hand-paper',
+      beats: 'Rock'
+    },
+    {
+      name: 'Scissors',
+      icon: 'fa-hand-scissors',
+      beats: 'Paper'
+    }
+  ];
+
+  // Show the choices the user can make as buttons
+  choices.forEach(choice => {
+    const choiceElm = document.createElement('button');
+    choiceElm.id = choice.name;
+    choiceElm.classList.add('far');
+    choiceElm.classList.add(choice.icon);
+    section.appendChild(choiceElm);
   });
 
+  // Handle the "game logic" when the user clicks a button
   section.addEventListener('click', e => {
-    console.log("Section clicked!");
+    // Which choice did we make?
+    const choice = choices.find(c => c.name == e.target.id);
+    if ( !choice ) return;
+    userChoice.innerHTML = `You choose <span>${choice.name.toUpperCase()}</span>`;
+    compChoice.innerHTML = '';
+    result.innerHTML = '';
+
+    // What choice does the computer make?
+    const computer = choices[Math.floor(Math.random() * choices.length)];
+    compChoice.innerHTML = `Computer chooses <span>${computer.name.toUpperCase()}</span>`
+    // window.setTimeout(() => compChoice.innerHTML =
+    //   `Computer chooses <span>${computer.name.toUpperCase()}</span>`, 1000);
+
+    // Who won?
+    // window.setTimeout(() => {
+      if ( choice.beats == computer.name ) {
+        result.innerHTML = 'You Win!';
+        scores.human++;
+      } else if ( computer.beats == choice.name ) {
+        result.innerHTML = 'Computer Wins!';
+        scores.computer++;
+      } else {
+        result.innerHTML = 'You tied!';
+      }
+
+      // Update scoreboard
+      document.getElementById("computer_score").innerHTML = scores.computer;
+      document.getElementById("user_score").innerHTML = scores.human;
+    // }, 1500);
   });
 
-  button.addEventListener('click', e => {
-    // e.stopPropagation();
-
-    // Optioneel:
-    // e.preventDefault() ?
-    // e.stopImmediatePropagation() ?
-
-    count += 1;
-    counter.innerText = count;
-    console.log("Button clicked!");
-  });
 });
